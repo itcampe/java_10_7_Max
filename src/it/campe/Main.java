9b3650704de3d2b6ccf654f37bc4a825ca06932b
Ksenx73 <hello@itcampe.com>
2021-10-21 16:22:23 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,10 @@
+package it.campe;
+
+public class Main {
+
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    }
+}
